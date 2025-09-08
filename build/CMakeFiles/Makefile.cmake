2f6410a5bd6441d6c45383d69781e8d82fe80b25
baobuibk <buiqbao@gmail.com>
2025-09-08 10:47:47 +0700

    feat:(build): add cppcheck target
    Add static analysis using cppcheck

--- build/CMakeFiles/Makefile.cmake
+++ build/CMakeFiles/Makefile.cmake
@@ -44,4 +44,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
   "CMakeFiles/EMBEDDED_TEST.dir/DependInfo.cmake"
+  "CMakeFiles/cppcheck.dir/DependInfo.cmake"
   )
